9e98b9f3fd4c4784ff1ccdfff859f6f4d6ea4216
voron <mriynuk@gmail.com>
2017-02-11 11:34:33 +0200

    Added Profile inline to User admin.

--- travel/admin.py
+++ travel/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
 from travel.models import Profile, Country
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(AuthUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Profile)
 admin.site.register(Country)
