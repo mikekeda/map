58796afb170bae3de93ee1ae20c7871913fbb987
Mykhailo Keda <mriynuk@gmail.com>
2017-10-17 20:06:24 +0300

    Improved error handling.

--- travel/views.py
+++ travel/views.py
@@ -110,7 +110,7 @@ class ApiView(View):
 
                 response = {'countries': countries}
         except urllib.error.URLError as e:
-            response['error'] = e
+            response['error'] = e.code + ': ' + e.reason
 
         # return JsonResponse(response)
         response = HttpResponse(json.dumps(response))
