e9d31def63bc3ef9e66ffcdc0236974f97608915
Mykhailo Keda <mriynuk@gmail.com>
2017-11-10 08:50:36 +0200

    Improved the test.

--- travel/tests.py
+++ travel/tests.py
@@ -4,6 +4,15 @@ from django.test import TestCase
 class LoanedBookInstancesByUserListViewTest(TestCase):
 
     # Pages available for anonymous.
-    def test_home_page(self):
+    def test_countries_endpoint(self):
+        # GET.
         resp = self.client.get('/api/countries')
+        self.assertEqual(resp.status_code, 200)
+        self.assertJSONEqual(
+            str(resp.content, encoding='utf8'),
+            {'countries': []}
+        )
+
+        # OPTIONS.
+        resp = self.client.options('/api/countries')
         self.assertEqual(resp.status_code, 200)
